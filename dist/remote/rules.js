f44b6cb18f1bdf6efd0abbaf9465366a71541c7c
liam <mcmanussliam@gmail.com>
2026-05-20 22:58:41 +0100

    Revert "chore(release): v1.0.0"
    This reverts commit 6dfc39ab703a6f067134e5c8f98f1b7d9112c54a.

--- dist/remote/rules.js
+++
@@ -1,5 +0,0 @@
-import fg from 'fast-glob';
-export async function collectProjectFacts(projectDir) {
-    const files = new Set(await fg('**/*', { cwd: projectDir, onlyFiles: true, deep: 4, dot: true, ignore: ['node_modules/**', '.git/**'] }));
-    return { files };
-}
